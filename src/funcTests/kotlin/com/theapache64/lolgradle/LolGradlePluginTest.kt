bc4f883cdd1adc57f5e9ce74ab14a3faed7d6716
theapache64 <theapache64@gmail.com>
2020-05-24 15:08:05 +0530

    ☑️ add funcTest

--- src/funcTests/kotlin/com/theapache64/lolgradle/LolGradlePluginTest.kt
+++ src/funcTests/kotlin/com/theapache64/lolgradle/LolGradlePluginTest.kt
@@ -1,19 +1,64 @@
+@file:Suppress("FunctionName")
+
 package com.theapache64.lolgradle
 
 
-import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
+import com.winterbe.expekt.should
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
 
 /**
  * Created by theapache64 : May 24 Sun,2020 @ 12:51
  */
 class LolGradlePluginTest {
+
+
     @get:Rule
     val testProjectDir = TemporaryFolder()
 
-    @Before
-    fun setUp(){
+    private lateinit var buildGradleFile: File
+    private lateinit var gradleRunner: GradleRunner
 
+    @Before
+    fun setUp() {
+        buildGradleFile = testProjectDir.newFile("build.gradle")
+
+        // Adding lol-gradle plugin
+        buildGradleFile.appendText(
+            """
+            plugins {
+                id 'java'
+                id 'com.theapache64.lol-gradle'
+            }
+        """.trimIndent()
+        )
+
+        gradleRunner = GradleRunner.create()
+            .withPluginClasspath()
+            .withProjectDir(testProjectDir.root)
+            .withTestKitDir(testProjectDir.newFolder())
+    }
+
+    @Test
+    fun `check test setup`() {
+        val result = gradleRunner.withArguments("tasks")
+            .build()
+
+        println(result.output)
+    }
+
+    @Test
+    fun `Capture`() {
+        val result = gradleRunner
+            .withArguments(LolGradlePlugin.TASK_CAPTURE)
+            .build()
+
+        result.task(":${LolGradlePlugin.TASK_CAPTURE}")!!.outcome.should.equal(TaskOutcome.SUCCESS)
+        result.output.should.contain("webcam(s) available")
     }
 }
