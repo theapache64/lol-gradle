c474d51a5b615f942ddf71d5ffe89716169a210a
theapache64 <theapache64@gmail.com>
2020-05-24 13:19:44 +0530

    ◀️ backup

--- src/funcTests/kotlin/com/theapache64/lolgradle/LolGradlePluginTest.kt
+++ src/funcTests/kotlin/com/theapache64/lolgradle/LolGradlePluginTest.kt
@@ -1,11 +1,19 @@
 package com.theapache64.lolgradle
 
-import org.gradle.internal.impldep.org.junit.Rule
+
+import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Created by theapache64 : May 24 Sun,2020 @ 12:51
  */
 class LolGradlePluginTest {
     @get:Rule
+    val testProjectDir = TemporaryFolder()
 
+    @Before
+    fun setUp(){
+
+    }
 }
